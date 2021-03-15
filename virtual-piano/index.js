d16a3a1a03f4937f50f4f6f17074f96296834251
Eugene-Smirnov <SmirnovEuV@gmail.com>
2021-03-16 01:26:38 +0300

    feat: add piano mouse events

--- virtual-piano/index.js
+++ virtual-piano/index.js
@@ -1,3 +1,5 @@
+// NOTATION SWITCHER
+
 const buttonNotes = document.querySelector('.btn-notes');
 const buttonLetters = document.querySelector('.btn-letters');
 const buttonContainer = document.querySelector('.btn-container');
@@ -8,8 +10,52 @@ function notationSwitcher(event) {
     if (!button || event.target.classList.contains('btn-active')) return;
     buttonNotes.classList.toggle('btn-active');
     buttonLetters.classList.toggle('btn-active');
-    keys.forEach(key => key.classList.toggle('notes'));
-    keys.forEach(key => key.classList.toggle('letters'));
+    keys.forEach(key => {
+        key.classList.toggle('notes'); 
+        key.classList.toggle('letters');
+    });
 };
 
 buttonContainer.addEventListener('click', notationSwitcher);
+
+// // PIANO
+// AUDIO
+
+// MOUSE EVENTS
+const piano = document.querySelector('.piano');
+
+let isMouseDown = false;
+function isMouseDownSwitch() {
+    isMouseDown = !isMouseDown;
+};
+
+window.addEventListener('mousedown', isMouseDownSwitch);
+window.addEventListener('mouseup', isMouseDownSwitch);
+
+function keyMouseUpDown(event) {
+    let key = event.target.closest('.piano-key');
+    key.classList.toggle('piano-key-active');   
+};
+
+piano.addEventListener('mousedown', keyMouseUpDown);
+piano.addEventListener('mouseup', keyMouseUpDown);
+
+function keyMouseOver(event) {
+    if (!event.target) return;
+    let keyOver = event.target.closest('.piano-key');
+    if (isMouseDown) {
+        keyOver.classList.toggle('piano-key-active');
+    };
+};
+
+piano.addEventListener('mouseover', keyMouseOver);
+
+function keyMouseOut(event) {
+    if (!event.target) return;
+    let keyOut = event.target.closest('.piano-key');
+    if (isMouseDown) {
+        keyOut.classList.toggle('piano-key-active');
+    };
+};
+
+piano.addEventListener('mouseout', keyMouseOut);
