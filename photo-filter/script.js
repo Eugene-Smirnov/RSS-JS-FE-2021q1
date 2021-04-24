e89a8ee13f03e986f5ef7645fd3a4cc83f9dd250
Eugene-Smirnov <SmirnovEuV@gmail.com>
2021-04-24 14:25:16 +0300

    feat: chain input value to filter css-values and change page fonts, style of input:active

--- photo-filter/script.js
+++ photo-filter/script.js
@@ -4,3 +4,12 @@ function outputHandler(event) {
   input.nextElementSibling.value = input.value;
 }
 filters.addEventListener("input", outputHandler);
+
+function filterValueChanger(event) {
+  if (event.target.name == undefined || event.target.name == "result") return;
+  const input = event.target;
+  const sizing = input.dataset.sizing;
+  document.documentElement.style.setProperty(`--${input.name}`, input.value + sizing);
+}
+
+filters.addEventListener("input", filterValueChanger);
