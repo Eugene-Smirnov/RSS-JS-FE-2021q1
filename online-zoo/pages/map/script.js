e0881e9d86799a819c4dcf1daf9ce28814eb6bcc
Eugene-Smirnov <SmirnovEuV@gmail.com>
2021-04-29 00:27:01 +0300

    feat: implement map-page dark theme

---
+++ online-zoo/pages/map/script.js
@@ -0,0 +1,42 @@
+/* THEME SWITHCER */
+
+const themeSwitcher = document.querySelector('.switch__checkbox');
+const map = document.querySelector('.map_dark');
+function setTheme() {
+  if (localStorage.darkThemeState !== undefined) {
+    if (localStorage.getItem('darkThemeState')) {
+      // Set Dark
+      document.documentElement.style.setProperty('--base-color', '#333');
+      document.documentElement.style.setProperty('--contrast-base-color', '#fefefe');
+      document.documentElement.style.setProperty('--additional-color', '#4f4f4f');
+      document.documentElement.style.setProperty('--contrast-additional-color', '#f2f2f2');
+      document.documentElement.style.setProperty('--map-bg-color', '#4f4f4f');
+      map.style.setProperty('opacity', '1');
+      themeSwitcher.checked = true;
+    } else {
+      // Set Light
+      document.documentElement.style.setProperty('--base-color', '#fefefe');
+      document.documentElement.style.setProperty('--contrast-base-color', '#333');
+      document.documentElement.style.setProperty('--additional-color', '#f2f2f2');
+      document.documentElement.style.setProperty('--contrast-additional-color', '#4f4f4f');
+      document.documentElement.style.setProperty('--map-bg-color', '#fefefe');
+      map.style.setProperty('opacity', '0');
+      themeSwitcher.checked = false;
+    }
+  }
+}
+setTheme();
+
+function themeSwith() {
+  if (localStorage.darkThemeState === undefined) {
+    localStorage.setItem('darkThemeState', '');
+  }
+  if (localStorage.getItem('darkThemeState')) {
+    localStorage.setItem('darkThemeState', '');
+    setTheme();
+  } else {
+    localStorage.setItem('darkThemeState', '1');
+    setTheme();
+  }
+}
+themeSwitcher.addEventListener('click', themeSwith);
