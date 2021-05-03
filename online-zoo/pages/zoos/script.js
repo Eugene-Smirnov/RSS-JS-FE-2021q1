fac4518fcc6e72ba07f6ecde917bd21b75982457
Eugene-Smirnov <SmirnovEuV@gmail.com>
2021-05-03 19:28:14 +0300

    refactor: separated themeSwithcer into module

--- online-zoo/pages/zoos/script.js
+++ online-zoo/pages/zoos/script.js
@@ -1,43 +1,6 @@
 /* THEME SWITHCER */
-
-const themeSwitcher = document.querySelector('.switch__checkbox');
-function setTheme() {
-  if (localStorage.darkThemeState !== undefined) {
-    if (localStorage.getItem('darkThemeState')) {
-      // Set Dark
-      document.documentElement.style.setProperty('--base-color', '#333');
-      document.documentElement.style.setProperty('--contrast-base-color', '#fefefe');
-      document.documentElement.style.setProperty('--additional-color', '#4f4f4f');
-      document.documentElement.style.setProperty('--contrast-additional-color', '#f2f2f2');
-      document.documentElement.style.setProperty('--map-bg-color', '#4f4f4f');
-      themeSwitcher.checked = true;
-    } else {
-      // Set Light
-      document.documentElement.style.setProperty('--base-color', '#fefefe');
-      document.documentElement.style.setProperty('--contrast-base-color', '#333');
-      document.documentElement.style.setProperty('--additional-color', '#f2f2f2');
-      document.documentElement.style.setProperty('--contrast-additional-color', '#4f4f4f');
-      document.documentElement.style.setProperty('--map-bg-color', '#fefefe');
-      themeSwitcher.checked = false;
-    }
-  }
-}
-setTheme();
-
-function themeSwith() {
-  if (localStorage.darkThemeState === undefined) {
-    localStorage.setItem('darkThemeState', '');
-  }
-  if (localStorage.getItem('darkThemeState')) {
-    localStorage.setItem('darkThemeState', '');
-    setTheme();
-  } else {
-    localStorage.setItem('darkThemeState', '1');
-    setTheme();
-  }
-}
-themeSwitcher.addEventListener('click', themeSwith);
-
+import {themeSwitcher, themeSwitch} from '../../scripts/themeSwitcher.js';
+themeSwitcher.addEventListener('click', themeSwitch);
 /* Video preview switcher */
 const video = document.querySelector('.video');
 const videoSelectors = document.querySelector('.video-selection__wrapper');
