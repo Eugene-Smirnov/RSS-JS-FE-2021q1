5c6616e7bab63d456d4c67f5a81a5b3ba20bf526
Eugene-Smirnov <SmirnovEuV@gmail.com>
2021-05-31 15:29:19 +0300

    delete: remove fake users

--- src/components/score/score.ts
+++ src/components/score/score.ts
@@ -3,11 +3,6 @@ import { BaseComponent } from '../base-component';
 import { ScoreElement } from './scoreElement';
 import { userService } from '../../services/user-service';
 import { User } from '../../models/user';
-import {
-  fakeUser1,
-  fakeUser2,
-  fakeUser3,
-} from '../../services/user-service/fake-users';
 
 export class Score extends BaseComponent {
   private users?: User[];
@@ -18,14 +13,6 @@ export class Score extends BaseComponent {
     <h2 class="score__heading">Best players</h2>
     `;
     userService.getTopPlayers().then((users) => {
-      if (users[0] === undefined) {
-        userService.userRepo.create(fakeUser1);
-        users.push(fakeUser1);
-        userService.userRepo.create(fakeUser2);
-        users.push(fakeUser2);
-        userService.userRepo.create(fakeUser3);
-        users.push(fakeUser3);
-      }
       users.forEach((user) => {
         const row = new ScoreElement(user);
         this.element.append(row.element);
