ab6d09922c8ee9ecb6be8d08a105f0b8046aeb7a
Eugene-Smirnov <SmirnovEuV@gmail.com>
2021-07-14 14:09:32 +0300

    feat: add log out button to admin header

--- english-for-kids-base/src/components/admin-panel/admin-header/admin-header.tsx
+++ english-for-kids-base/src/components/admin-panel/admin-header/admin-header.tsx
@@ -1,10 +1,18 @@
-import { FC } from 'react';
+import { FC, useCallback } from 'react';
+import { useHistory } from 'react-router-dom';
 import './admin-header.scss';
 
 export const AdminHeader: FC = () => {
+  const history = useHistory();
+  const onLogOut = useCallback(() => {
+    history.push('/');
+  }, [history]);
   return (
     <header className="admin-header">
       <h2 className="heading">admin panel</h2>
+      <button className="admin-header__button" onClick={onLogOut}>
+        log out
+      </button>
     </header>
   );
 };
