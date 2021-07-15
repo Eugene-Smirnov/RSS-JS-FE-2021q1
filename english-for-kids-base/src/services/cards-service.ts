5aeca2900c5f0eb843203ec083dedd6eaaeee2aa
Eugene-Smirnov <SmirnovEuV@gmail.com>
2021-07-15 20:48:21 +0300

    feat: add CRUD methods in front card-service

--- english-for-kids-base/src/services/cards-service.ts
+++ english-for-kids-base/src/services/cards-service.ts
@@ -1,10 +1,54 @@
 import { CardModel } from '../models/card-model';
-import { CATEGORIES_URL } from './server-specs';
+import { LOCALSTORAGE_TOKEN_NAME } from './auth-service';
+import { createCardUrl } from './server-specs';
 
 export const cardsService = {
   async getCards(categoryId: string): Promise<CardModel[]> {
-    const response = await fetch(`${CATEGORIES_URL}/${categoryId}/card`);
+    const response = await fetch(createCardUrl(categoryId));
     const data: CardModel[] = await response.json();
     return data;
+  },
+
+  async create(categoryId: string): Promise<CardModel> {
+    const response = await fetch(createCardUrl(categoryId), {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${window.localStorage.getItem(LOCALSTORAGE_TOKEN_NAME)}`,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        name: Date.now().toString(),
+        title: 'New Card',
+        image: '../images/EFK-base-image.jpg',
+        audio: '',
+        translation: 'Новая карта',
+      }),
+    });
+    const newCard: CardModel = await response.json();
+    return newCard;
+  },
+
+  async update(updatedCard: CardModel): Promise<CardModel> {
+    const response = await fetch(`${createCardUrl(updatedCard.categoryId)}/${updatedCard.id}`, {
+      method: 'PUT',
+      headers: {
+        Authorization: `Bearer ${window.localStorage.getItem(LOCALSTORAGE_TOKEN_NAME)}`,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(updatedCard),
+    });
+    const card: CardModel = await response.json();
+    return card;
+  },
+
+  async remove(card: CardModel): Promise<CardModel> {
+    const response = await fetch(`${createCardUrl(card.categoryId)}/${card.id}`, {
+      headers: {
+        Authorization: `Bearer ${window.localStorage.getItem(LOCALSTORAGE_TOKEN_NAME)}`,
+      },
+      method: 'DELETE',
+    });
+    const deletedCard: CardModel = await response.json();
+    return deletedCard;
   },
 };
--- english-for-kids-base/src/services/server-specs.ts
+++ english-for-kids-base/src/services/server-specs.ts
@@ -1,4 +1,8 @@
 export const SERVER_URL = 'http://localhost:3001/api';
+
 export const CATEGORIES_URL = `${SERVER_URL}/category`;
+
+export const createCardUrl = (categoryId: string): string => `${CATEGORIES_URL}/${categoryId}/card`;
+
 export const AUTH_URL = `${SERVER_URL}/auth`;
 export const LOGIN_URL = `${AUTH_URL}/login`;
